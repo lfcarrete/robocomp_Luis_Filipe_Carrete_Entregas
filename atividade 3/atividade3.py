94af9f7394eb0c36acc8032d545d9c4d0e147ece
Luis Filipe Carrete <lfcsanchez@live.com>
2020-03-05 17:53:54 -0300

    .

--- atividade 3/atividade3.py
+++ atividade 3/atividade3.py
@@ -3,13 +3,29 @@ import numpy as np
 from matplotlib import pyplot as plt
 import time
 import math
+import statistics
 
 
-teste = cv2.imread('teste.png')
 
-#teste_gray = cv2.cvtColor(teste, cv2.COLOR_BGR2GRAY)
+def fuga (img):
+    
+    teste = cv2.imread(img)
 
-#mascara = cv2.inRange(teste_gray,245,255)
+    teste_gray = cv2.cvtColor(teste, cv2.COLOR_BGR2GRAY)
 
-#cv2.imshow(mascara)
+    mascara = cv2.inRange(teste_gray,250,255)
 
+    #lines = cv2.HoughLinesP(mascara, 10, math.pi/180.0, 100, np.array([]), 45, 5)
+
+    #a,b,c = lines.shape
+
+    #for i in range(a):
+        # Faz uma linha ligando o ponto inicial ao ponto final, com a cor vermelha (BGR)
+     #   cv2.line(mascara, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), (0, 255, 0), 5, cv2.LINE_AA)
+    
+    plt.imshow(mascara)
+    
+    
+
+
+fuga('teste.png')
