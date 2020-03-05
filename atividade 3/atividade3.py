929d822b96c3e182adbacc8b5797f04fff759ce1
Luis Filipe Carrete <lfcsanchez@live.com>
2020-03-05 16:52:49 -0300

    atividade 3

---
+++ atividade 3/atividade3.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import time
+import math
+
+
+teste = cv2.imread('teste.png')
+
+#teste_gray = cv2.cvtColor(teste, cv2.COLOR_BGR2GRAY)
+
+#mascara = cv2.inRange(teste_gray,245,255)
+
+#cv2.imshow(mascara)
+
