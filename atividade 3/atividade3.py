e21bb8f37b510f7e0d158bd94bce2a3a623c284c
Luis Filipe Carrete <lfcsanchez@live.com>
2020-03-08 15:14:41 -0300

    .

--- atividade 3/atividade3.py
+++ atividade 3/atividade3.py
@@ -5,27 +5,64 @@ import time
 import math
 import statistics
 
-
+def reta (x1,y1,x2,y2):
+    m = (x2 - x1)/(y2 - y1)
+    
+    y = m * x2 - m * x1 + y2
+    
+    return y
+    
 
 def fuga (img):
     
-    teste = cv2.imread(img)
+   # img = cv2.imread(img)
 
-    teste_gray = cv2.cvtColor(teste, cv2.COLOR_BGR2GRAY)
+    teste_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     mascara = cv2.inRange(teste_gray,250,255)
-
-    #lines = cv2.HoughLinesP(mascara, 10, math.pi/180.0, 100, np.array([]), 45, 5)
-
-    #a,b,c = lines.shape
-
-    #for i in range(a):
-        # Faz uma linha ligando o ponto inicial ao ponto final, com a cor vermelha (BGR)
-     #   cv2.line(mascara, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), (0, 255, 0), 5, cv2.LINE_AA)
     
-    plt.imshow(mascara)
-    
+    # Apply edge detection method on the image 
+    edges = cv2.Canny(mascara,100,255,apertureSize = 5) 
+
+    # Detect points that form a line
+
+    lines = cv2.HoughLinesP(edges, 10, math.pi/180.0, 100, np.array([]), 45, 5)
+
+    # Draw lines on the image
+
+    for line in lines:
+
+        x1, y1, x2, y2 = line[0]
+        
+        
+        cv2.line(img, (x1,y1), (x2, y2), (255, 0, 0), 3)
+        
+    #Show result
+    return(img)
     
 
+#plt.imshow(fuga('teste.png'))
+    
+video_capture = cv2.VideoCapture("vid1.mp4")
+while True:
+    ret, frame = video_capture.read()
+    if ret:
+        
+        cv2.imshow('Video', fuga(frame))
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+    
+video_capture.release()
+cv2.destroyAllWindows()
 
-fuga('teste.png')
+
+
+
+
+
+
+
+
